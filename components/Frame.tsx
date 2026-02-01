be60bcce7f4716fd9982d9474e0a28a60001383f
tzptzptzptzptzp <git@tzp.pink>
2026-02-01 23:55:39 +0900

    ✨ PC用のフレームを改善

--- components/Frame.tsx
+++ components/Frame.tsx
@@ -1,6 +1,12 @@
 export const Frame = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="contents md:block overflow-hidden w-sm max-h-full rounded-2xl">
+    <div
+      className="contents md:block overflow-hidden w-sm max-h-full rounded-2xl"
+      style={{
+        boxShadow:
+          "2px 2px 5px 3px rgba(0, 0, 0, 0.05), 4px 4px 25px 5px rgba(0, 0, 50, 0.15)",
+      }}
+    >
       {children}
     </div>
   );
