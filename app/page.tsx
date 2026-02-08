cb902392d63d95f3b57f756fd7c6869c9d5bf327
tzptzptzptzptzp <git@tzp.pink>
2026-02-08 11:27:46 +0900

    ♻️ 美少女ちゃんをコンポーネントに切り出し

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
+import { CharacterImage } from "@/components/CharacterImage";
 import { DialogueArea } from "@/components/DialogueArea";
 import { Frame } from "@/components/Frame";
-import Image from "next/image";
-
-const IMG_URL = "/bisyojo_chan.png";
-const IMG_SIZE = 360;
 
 export default function Home() {
   return (
@@ -11,14 +8,7 @@ export default function Home() {
       <Frame>
         <div className="flex flex-col relative w-full h-screen bg-main">
           <DialogueArea />
-
-          <Image
-            className="absolute right-0 bottom-0 left-0 w-full h-auto pointer-events-none"
-            src={IMG_URL}
-            width={IMG_SIZE}
-            height={IMG_SIZE}
-            alt="美少女ちゃん"
-          />
+          <CharacterImage />
         </div>
       </Frame>
     </main>
