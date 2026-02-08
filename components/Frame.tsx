a72b87c404bd0e30ed3bc622a8dd37db7db54ba0
tzptzptzptzptzp <git@tzp.pink>
2026-02-08 11:59:31 +0900

    💄 Frameのスタイルを調整

--- components/Frame.tsx
+++ components/Frame.tsx
@@ -1,12 +1,6 @@
 export const Frame = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div
-      className="contents md:block overflow-hidden w-sm max-h-full rounded-2xl"
-      style={{
-        boxShadow:
-          "2px 2px 5px 3px rgba(0, 0, 0, 0.05), 4px 4px 25px 5px rgba(0, 0, 50, 0.15)",
-      }}
-    >
+    <div className="contents md:block overflow-hidden w-sm max-h-full">
       {children}
     </div>
   );
