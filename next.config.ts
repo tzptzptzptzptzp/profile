539af13a87a1df8a13113f541fb474acadaa2d06
tzptzptzptzptzp <git@tzp.pink>
2026-02-09 10:57:57 +0900

    🔧 静的HTML生成の設定を追加

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
   images: {
     domains: ["bsj-api.vercel.app"],
+    unoptimized: true,
   },
 };
 
